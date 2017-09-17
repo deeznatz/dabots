30a103ec777a87afba8a74c10625c6a6ed14e4a7
deeznatz <31816062+deeznatz@users.noreply.github.com>
2017-09-17 18:06:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,20 +6,6 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', function(message) {
-   if (message.author.equals(client.user)) return;
-  
-  if  (!message.content.StartsWith(PREFIX)) return;
-  var args = message.content.substring(PREFIX.length).split(" ");
-  
-  switch (args[0]) {
-     case "Komandos"
-           message.channel.sendMessage("Komandos:
-      !Komandos");
-           break;
-  }
-});
-
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
