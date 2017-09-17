52f87f6d865008ab784fbbabccc59d5c1e935176
deeznatz <31816062+deeznatz@users.noreply.github.com>
2017-09-17 17:59:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,23 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const PREFIX - "&";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', function(message) {
+   if (message.author.equals(client.user)) return;
+  
+  if  (!message.content.StartsWith(PREFIX)) return;
+  var args = message.content.substring(PREFIX.length).split(" ");
+  
+  switch (args[0]) {
+     case "Komandos"
+           message.channel.sendMessage("Komandos:
+      !Komandos");
+           break;
+  }
 });
 
 client.on('message', msg => {
