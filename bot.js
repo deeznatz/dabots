f7fee71e3e56d0ed2342b5542865d5f116f24b0f
deeznatz <31816062+deeznatz@users.noreply.github.com>
2017-09-18 20:37:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,35 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const bot = new Discord.Client();
 const PREFIX = "&";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+bot.on("message", function(message) {
+  if (message.author.equals(bot.user)) return;
+
+  if (!message.content.startsWith(PREFIX)) return;
+
+  var args = message.content.substring(PREFIX.length).split(" ");
+
+  switch (args[0]) {
+    case "ping":
+      message.channel.sendMessage("Pong!")
+      break;
+    case "make":
+      message.reply("memes")
+      break;
+    case "Komandos":
+      message.author.sendMessage(":thinking:")
+    case "Taisykles":
+      var embed = new Discord.RichEmbed()
+          .addField("1. Nesityčioti.", "1 kartas - warning, 2 kartas - mute 3 dienai, 3 kartas - ban visam laikui. ")
+          .addField("2. Nefloodinti chat'o", "1 kartas - kick, 2 kartas - kartas mute 1 dienai, 3 kartas - ban 7 dienom. ")
+      message.channel.sendEmbed(embed);
+      break;
+      
+      
   }
 });
 
